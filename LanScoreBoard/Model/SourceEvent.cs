4db296749022441398db540ff81602ca0a3b6e5c
K47S <michi.volz@gmx.net>
2017-09-30 18:17:22 +0200

    Fix: Remove dupliyte fields

--- LanScoreBoard/Model/SourceEvent.cs
+++ LanScoreBoard/Model/SourceEvent.cs
@@ -4,21 +4,11 @@ namespace LanScoreBoard.Model
 {
     public class SourceEvent : EventBase
     {       
-        public int Id { get; set; }
-
-        [JsonProperty("name")]
-        public string Name { get; set; }
+       
 
         [JsonProperty("steamId")]
         public int SteamId { get; set; }
 
-        [JsonProperty("event")]
-        public string Event { get; set; }
-
-        [JsonProperty("game")]
-        public string Game { get; set; }
-
-        [JsonProperty("timestamp")]
-        public string Timestamp { get; set; }
+        
     }
 }
