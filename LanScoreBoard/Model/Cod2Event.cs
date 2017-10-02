2355af4507ed9c5c424cf621bbfed84c8c604527
K47S <michi.volz@gmx.net>
2017-10-02 16:43:28 +0200

    Update Cod2Event

--- LanScoreBoard/Model/Cod2Event.cs
+++ LanScoreBoard/Model/Cod2Event.cs
@@ -1,6 +1,17 @@
-﻿namespace LanScoreBoard.Model
+﻿using Newtonsoft.Json;
+
+namespace LanScoreBoard.Model
 {
     public class Cod2Event : EventBase
     {
+
+        [JsonProperty("addedScore")]
+        public int AddedScore { get; set; }
+
+        [JsonProperty("guid")]
+        public string Guid { get; set; }
+
+        [JsonProperty("adress")]
+        public string Adress { get; set; }
     }
 }
